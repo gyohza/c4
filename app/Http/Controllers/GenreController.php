2919c36dd616e44d49e2fa9ef419acc243a285a7
dan <dan.nagaoka.0@gmail.com>
2020-10-03 22:02:42 -0300

    Delegate service injection to Controller class

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,22 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\GenreResource;
-use App\Services\TMDBService;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
-    /**
-     * Service to consume TMDB API.
-     * @var TMDBService
-     */
-    public TMDBService $tmdb;
-
-    public function __construct(TMDBService $tmdbService)
-    {
-        $this->tmdb = $tmdbService;
-    }
-
     /**
      * Display a listing of the resource.
      *
